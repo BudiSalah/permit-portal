d0ead848303ed3c5c521aa4c7baeb0d01e9d0b68
Budi Salah <a.salah@awaed.co>
2025-11-07 20:46:38 +0200

    Remove Tailwind CSS configuration file as part of project cleanup.

--- nuxt-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./app/**/*.{js,vue,ts}', './components/**/*.{js,vue,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
